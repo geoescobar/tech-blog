a8b58a425e26778700580cec0330fa607d65985a
George Escobar <geo.escobar214@gmail.com>
2022-06-16 17:45:02 -0500

    css update

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,27 +4,9 @@ const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
   try {
-    // Get all projects and JOIN with user data
-    const projectData = await Posts.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ["name"],
-        },
-      ],
-    });
-
-    // Serialize data so the template can read it
-    const projects = projectData.map((project) => project.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
-    res.render("homepage", {
-      projects,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
+    res.render("homepage");
+  } catch (error) {
+    res.json(error);
   }
 });
 
@@ -78,6 +60,15 @@ router.get("/login", (req, res) => {
   }
 
   res.render("login");
+});
+
+
+router.get("/signup", async (req, res) => {
+  try {
+    res.render("signup");
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 module.exports = router;
--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,53 @@
-console.log(test);
+console.log("test");
+
+// ================================ login ================================
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+
+  const email = document.getElementById("loginemail").value.trim();
+  const password = document.getElementById("loginpwd").value.trim();
+
+  if (email && password) {
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert("Failed to log in");
+    }
+  }
+};
+
+document
+  .getElementById("signinbttn")
+  ?.addEventListener("click", loginFormHandler);
+
+// ================================ sign-up ================================
+const signup = async (event) => {
+  event.preventDefault();
+
+  const username = document.getElementById("userinfoname").value.trim();
+  const password = document.getElementById("passwordsignup").value.trim();
+
+  if (email && password && fname && lname && username) {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({ email, password, lname, fname, username }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert("Failed to log in");
+    }
+  }
+};
+
+document.getElementById("signupbttn")?.addEventListener("click", signup);
+
+
